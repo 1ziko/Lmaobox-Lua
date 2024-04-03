0f7cf59b0286a42fb0b13b3fb36521506ad4c578
1ziko <165936287+1ziko@users.noreply.github.com>
2024-04-03 22:03:45 +0200

    Update AutoHitmansHeatmakerFocus.lua

--- Luas/AutoHitmansHeatmakerFocus.lua
+++ Luas/AutoHitmansHeatmakerFocus.lua
@@ -1,25 +1,8 @@
-
-local consolas = draw.CreateFont("Consolas", 17, 500)
-
-/* Uncomment if you want "AutoFocus Activated text under your crosshair" */
-/*
-local function displayStatus()
-    draw.SetFont(consolas)
-    draw.Color(255, 255, 255, 255)
-    draw.Text(
-                //  My screen is 3440x1440
-        1650,   //  Half of your screen's width - 70
-        820,    //  Half of your screen's height + 100
-        "AutoFocus Activated")
-end
-callbacks.Register("Draw", "draw", displayStatus)
-*/
-
 local function onPlayerDeath(event)
-    if event:GetName() == "player_death" and event:GetInt("attacker") == client.GetLocalPlayerIndex() + 1 then
+    if event:GetName() == "player_death" and event:GetInt("attacker") -243 == client.GetLocalPlayerIndex() then
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> You")
         client.Command('+reload',true)
     end
-
 end
-
+callbacks.Unregister("FireGameEvent", "AutoTauntOnKill", onPlayerDeath)
 callbacks.Register("FireGameEvent", "AutoTauntOnKill", onPlayerDeath)
